8826a447b683a1ba4a8bf20804e4580e130f7bfc
rohitverse <rohitkr23072001@gmail.com>
2023-02-11 12:09:44 +0530

    logo added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+import images from '../assets';
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div>
+        <Link href="/">
+          <div className="flex flex-1 flex-row justify-start">
+            <div>
+              {' '}
+              <Image
+                src={images.logo02}
+                objectFit="contain"
+                width={32}
+                height={32}
+                alt="logo"
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

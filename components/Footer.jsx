0b1f0d335b7f764b45577e5a09bcad48a58a94b5
rohitverse <rohitkr23072001@gmail.com>
2023-02-11 20:56:24 +0530

    Footer Added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -74,7 +74,6 @@ const Footer = () => {
       <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray1 sm:px-4 px-16">
         <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
-            {' '}
             NFTGalaxy, Inc. All Rights Reserved.
           </p>
           <div className="flex flex-row sm:mt-4">

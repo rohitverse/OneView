172debc97e930123d235f58555834d0276e97679
rohitverse <rohitkr23072001@gmail.com>
2023-02-27 15:30:07 +0530

    added top nft

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,13 +1,9 @@
-import { useContext } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-// import { NFTContext } from '../context/NFTContext';
 
 import images from '../assets';
-// import { shortenAddress } from '../utils/shortenAddress';
 
 const NFTCard = ({ nft }) => (
-  //   const { nftCurrency } = useContext(NFTContext);
   <Link href={{ pathname: '/nft-details', query: nft }}>
     <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
       <div className="relative w-full h-52 sm:h-36 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
@@ -29,7 +25,6 @@ const NFTCard = ({ nft }) => (
           </p>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">
             0x62..oK
-            {/* {shortenAddress(onProfilePage ? nft.owner : nft.seller)} */}
           </p>
         </div>
       </div>
